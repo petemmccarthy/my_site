3cb1ce8e395f21dc354ee61a7629f91ab8b65de7
Pete M McCarthy <pete.m.mccarthy@gmail.com>
2015-07-18 12:56:49 +0100

    bootstrap removed

--- Brocfile.js
+++ Brocfile.js
@@ -17,12 +17,4 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import("bower_components/font-awesome/css/font-awesome.css");
-
-app.import("bower_components/bootstrap/dist/js/bootstrap.js");
-app.import("vendor/assets/css/bootstrap.css");
-app.import("vendor/assets/css/bootstrap-theme.css");
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-  destDir: 'assets'
-});
 module.exports = app.toTree();
